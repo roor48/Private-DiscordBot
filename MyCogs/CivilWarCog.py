dbf182e73d9be6e01096367eb8fdd801f694e634
roor48 <kmgoio@naver.com>
2025-01-08 19:04:28 +0900

    내전생성시 메시지 고정, 내전 종료시 메시지 수정 & 고정해재 기능 추가

--- MyCogs/CivilWarCog.py
+++ MyCogs/CivilWarCog.py
@@ -1,8 +1,12 @@
 import discord
 from discord.ext import commands, tasks
 from discord import app_commands
+from datetime import datetime, timezone
 import random
 import asyncio
+
+# todo
+# 내전종료시 reply 제거, 메시지 수정하는 방법으로
 
 class CivilWarCog(commands.Cog):
     def __init__(self, client):
@@ -10,7 +14,19 @@ class CivilWarCog(commands.Cog):
         self.createdWarUserId = {}
         self.createdWarMessageId = {}
         self.check_expired_wars.start()  # 주기적으로 만료된 내전 체크 시작
-    
+
+    async def on_close(self):
+        await self.clear_messages()
+
+    async def clear_messages(self):
+        user_ids = []
+        msg_ids = []
+        for user_id, data in self.createdWarUserId.items():
+            user_ids.append(user_id)
+            msg_ids.append(data["message"].id)
+        
+        for i in range(len(user_ids)):
+            await self.deleteCreateDict(user_ids[i], msg_ids[i])
 
     @commands.Cog.listener()
     async def on_reaction_add(self, reaction, user): # 반응이 추가됐을때 호출
@@ -33,38 +49,48 @@ class CivilWarCog(commands.Cog):
 
     @app_commands.command(name="내전생성", description="내전 인원을 모읍니다.")
     async def createCivilWar(self, interaction: discord.Interaction, message:str, max_player:int = 0, team_count:int = 2):
+        if len(message) > 200:
+            await interaction.response.send_message("message는 200자 이내로 작성해주세요.", ephemeral=True)
+            return
         if team_count <= 0:
-            await interaction.response.send_message("team_count는 1 이상이여야 합니다.")
+            await interaction.response.send_message("team_count는 1 이상이여야 합니다.", ephemeral=True)
+            return
+        if team_count > 100:
+            await interaction.response.send_message("team_count는 100 이하이여야 합니다.", ephemeral=True)
+            return
+        if max_player < 0:
+            max_player = 0
 
-        await interaction.response.send_message(f"{message}\n`최대인원: {'no_limits' if max_player==0 else max_player}` `팀 수: {team_count}`\n-# ✅반응을 눌러 내전에 참가하세요!")
+        await interaction.response.send_message(f"{message}\n`최대인원: {'no_limits' if max_player<=0 else max_player}` `팀 수: {team_count}`\n-# ✅반응을 눌러 내전에 참가하세요!")
         msg = await interaction.original_response()
         await msg.add_reaction("✅")
+        await msg.pin(reason="내전이 생성되었습니다.")
 
-        create_time = asyncio.get_event_loop().time()
+        if interaction.user.id in self.createdWarUserId:
+            await self.deleteCreateDict(interaction.user.id, self.createdWarUserId[interaction.user.id]['message'].id)
+
         self.createdWarUserId[interaction.user.id] = {
-            "channel_id": msg.channel.id,
-            "message_id": msg.id,
+            "message": msg, # Message 객체 (/내전생성 으로 봇이 보낸 메시지)
             "max_player": max_player,
             "team_count": team_count,
-            "create_time": create_time
         }
         self.createdWarMessageId[msg.id] = {
             "user_id": interaction.user.id,
         }
 
 
+
     
     @app_commands.command(name="내전종료", description="이전에 생성한 내전을 종료합니다.")
     async def removeCivilWar(self, interaction: discord.Interaction):
         if interaction.user.id not in self.createdWarUserId:
-            await interaction.response.send_message("생성된 내전이 없습니다.")
+            await interaction.response.send_message("생성된 내전이 없습니다.", ephemeral=True)
             return
 
         try:
-            message = await interaction.channel.fetch_message(self.createdWarUserId[interaction.user.id]["message_id"])
-
+            await self.deleteCreateDict(interaction.user.id, self.createdWarUserId[interaction.user.id]['message'].id)
             await interaction.response.send_message("생성된 내전이 종료되었습니다.", ephemeral=True)
-            await message.reply("생성된 내전이 종료되었습니다.")
+
         except discord.NotFound:
             await interaction.response.send_message("이 채널에서 메시지를 찾을 수 없습니다.", ephemeral=True)
         except discord.Forbidden:
@@ -72,17 +98,15 @@ class CivilWarCog(commands.Cog):
         except discord.HTTPException:
             await interaction.response.send_message("서버 오류가 발생했습니다.", ephemeral=True)
 
-        del self.createdWarMessageId[self.createdWarUserId[interaction.user.id]["message_id"]]
-        del self.createdWarUserId[interaction.user.id]
-
     @app_commands.command(name="팀뽑기", description="반응한 사람 중 팀 랜덤 배치")
     async def getTeam(self, interaction: discord.Interaction): # /팀뽑기 명령어
         try:
             if interaction.user.id not in self.createdWarUserId:
-                await interaction.response.send_message("생성된 내전이 없습니다.")
+                await interaction.response.send_message("생성된 내전이 없습니다.", ephemeral=True)
                 return
 
-            message = await interaction.channel.fetch_message(self.createdWarUserId[interaction.user.id]["message_id"])
+            # Message객체 새로 불러와야 됨
+            message = await interaction.channel.fetch_message(self.createdWarUserId[interaction.user.id]["message"].id)
             
             # 반응한 사용자들을 저장할 리스트
             reacted_users = []
@@ -99,7 +123,7 @@ class CivilWarCog(commands.Cog):
             reacted_users = list(set(reacted_users))
 
             if not reacted_users:
-                await interaction.response.send_message("✅ 이모지에 반응한 사람이 없습니다.")
+                await interaction.response.send_message("✅ 이모지에 반응한 사람이 없습니다.", ephemeral=True)
                 return
             
             random.shuffle(reacted_users)
@@ -133,33 +157,37 @@ class CivilWarCog(commands.Cog):
         except discord.HTTPException:
             await interaction.response.send_message("서버 오류가 발생했습니다.", ephemeral=True)
 
+    async def deleteCreateDict(self, user_id, message_id):
+        try:
+            await self.createdWarUserId[user_id]["message"].edit(content="이 내전은 종료되었습니다.")
+            await self.createdWarUserId[user_id]["message"].unpin(reason="내전이 종료되었습니다.")
+        except: ...
+
+        del self.createdWarUserId[user_id]
+        del self.createdWarMessageId[message_id]
 
     # 주기적으로 만료된 내전 데이터 확인
     @tasks.loop(minutes=30)  # 30분마다 체크
     async def check_expired_wars(self):
-        current_time = asyncio.get_event_loop().time()  # 현재 시간 (초 단위)
+        current_time = datetime.now(timezone.utc)
         expired_wars = []  # 만료된 내전 목록
         
+
         # 만료된 내전 데이터 찾기
         for user_id, data in self.createdWarUserId.items():
-            message_id = data["message_id"]
-            create_time = data.get("create_time")
-            if not create_time or (current_time - create_time >= 86400):  # 24시간 이상 경과하면
+            try:
+                create_time = data["message"].created_at
+                if (current_time - create_time).total_seconds() >= 86400:  # 24시간 이상 경과하면
+                    expired_wars.append(user_id)
+            except:
                 expired_wars.append(user_id)
 
         # 만료된 내전 삭제
         for user_id in expired_wars:
             try:
-                channel_id = self.createdWarUserId[user_id]["channel_id"]
-                message_id = self.createdWarUserId[user_id]["message_id"]
+                message = self.createdWarUserId[user_id]['message']
+                self.deleteCreateDict(user_id, message.id)
 
-                del self.createdWarUserId[user_id]
-                del self.createdWarMessageId[message_id]
-            
-                # 관련된 메시지에 24시간 후에 내전이 삭제됐다는 알림을 보내기
-                channel = self.client.get_channel(channel_id)
-                message = await channel.fetch_message(message_id)
-                await message.reply("이 내전은 24시간이 지나 자동으로 종료되었습니다.")
             except Exception as e:
                 print(f'Error While Auto-Deleting: {e}')
 
@@ -169,7 +197,20 @@ class CivilWarCog(commands.Cog):
             await interaction.response.send_message("이 기능은 관리자만 사용할 수 있습니다.", ephemeral=True)
             return
 
-        print(self.createdWarUserId)
-        print(self.createdWarMessageId)
+        import psutil
+        import os
 
-        await interaction.response.send_message("딕셔너리 출력 완료", ephemeral=True)
+        # 현재 프로세스의 PID 얻기
+        pid = os.getpid()
+
+        # psutil을 사용하여 프로세스 객체 얻기
+        process = psutil.Process(pid)
+
+        # 메모리 사용량 확인 (단위: 바이트)
+        memory_info = process.memory_info()
+
+        await interaction.response.send_message(f'`RSS (Resident Set Size): {memory_info.rss/1024/1024}mb`\n`VMS (Virtual Memory Size): {memory_info.vms/1024/1024/1024}gb`')
+
+        print(len(self.createdWarUserId))
+        print(len(self.createdWarMessageId))
+
--- main.py
+++ main.py
@@ -11,21 +11,34 @@ pip install -U discord.py
 """
 
 import os
-from dotenv import load_dotenv
+import sys
+
+import signal
+import asyncio
+
 import discord
 from discord.ext import commands
 from discord import app_commands
 
 from MyCogs import *
+
+from dotenv import load_dotenv
 load_dotenv()
 del load_dotenv
+
+# 종료 시 실행될 함수
+def on_bot_exit():
+    print("Bot is exiting...")
+
 
 class MyClient(commands.Bot):
     def __init__(self, *, intents: discord.Intents):
         super().__init__(command_prefix="!", intents=intents)
 
+
     async def on_ready(self):
         print(f'Logged on as {self.user}!')
+
 
         # SlashCommands Cog 등록
         await self.add_cog(CivilWarCog(self)) # 내전 관련 Cog
@@ -41,10 +54,21 @@ class MyClient(commands.Bot):
 
         print('slash command sync success')
 
-intents = discord.Intents.default()
-intents.message_content = True
-client = MyClient(intents=intents)
+    # 오버라이딩
+    async def close(self):
+        # 클린업 작업을 비동기적으로 수행한 후 봇을 종료합니다.
+        await self.cleanup()  # 비동기적으로 cleanup을 호출
+        await super().close()  # 그 후 기본 close 메서드를 호출하여 봇 종료
+
+    async def cleanup(self):
+        await self.get_cog("CivilWarCog").on_close()
 
 
+# 프로그램 시작
+if __name__ == "__main__":
+    intents = discord.Intents.default()
+    intents.message_content = True
+    client = MyClient(intents=intents)
 
-client.run(os.getenv('BOT_KEY'))
+    client.run(os.getenv('BOT_KEY'))
+
